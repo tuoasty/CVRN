b0cf370d8c24e5a5e08467ef8ca9185269c857bd
tuoasty <tuoasty@gmail.com>
2026-03-04 15:51:36 +0700

    Remove suspense

--- app/admin/teams/[region]/[season]/[teamName]/page.tsx
+++ app/admin/teams/[region]/[season]/[teamName]/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from "react";
 import TeamDetailPage from "@/app/features/teams/TeamDetailPage";
 
 export default function TeamPage() {
     return (
-        <Suspense fallback={<div className="admin-section"><div className="panel p-6"><p>Loading...</p></div></div>}>
-            <TeamDetailPage />
-        </Suspense>
+        <TeamDetailPage />
     );
 }
